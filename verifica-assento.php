bfc57ebbb723c383eabb452c0b0159f819174347
Maria Eduarda Pereira Mendonça <mariaeduarda382749@gmail.com>
2026-08-21 11:37:07 -0300

    Ajuste estético geral do projeto
    Remove sublinhado e cor padrão de link da tag <a>
    Ajusta outros detalhes visuais do CSS
    Mudança de nome dos arquivos

--- verifica-assento.php
+++ verifica-assento.php
@@ -6,7 +6,7 @@ $cadeiras = array(
     array(true, false, true, false, true),
     array(true, true, true, false, false)
 );
-$sugestoes = [];
+
 $mensagem= "";
 if (isset($_POST['fileira']) && isset($_POST['assento'])) {
 
@@ -34,11 +34,11 @@ if (isset($_POST['fileira']) && isset($_POST['assento'])) {
      <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="ossos.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-    <div class="container-cinema">
+    <div class="container-caixinha">
 
             <h1>Resultado</h1>
 
@@ -58,7 +58,7 @@ if (isset($_POST['fileira']) && isset($_POST['assento'])) {
             </div>
 
             <br>
-            <a href="carne.html"> Verificar outro assento</a>
+            <a href="index.html">۶۟ৎVerificar outro assento۶۟ৎ</a>
     </div>
 
 </body>
